453cf459f3404d8e86428fa5c636bf806171c31f
Lightlana888 <svetlana.yakuta@gmail.com>
2024-03-23 21:33:46 +0100

    space

--- src/components/heroCard.jsx
+++ src/components/heroCard.jsx
@@ -5,13 +5,13 @@ function HeroCard(props) {
     return (
         <div className='hero-card'>
             <h2><strong>{name}</strong></h2>
-            <p><strong>Вселенная:</strong>{universe}</p>
-            <p><strong>Альтерэго:</strong>{alterego}</p>
-            <p><strong>Профессия:</strong>{occupation}</p>
-            <p><strong>Друзья:</strong>{friends}</p>
-            <p><strong>Суперсила:</strong>{superpowers}</p>
+            <p><strong>Вселенная:</strong> {universe}</p>
+            <p><strong>Альтерэго:</strong> {alterego}</p>
+            <p><strong>Профессия:</strong> {occupation}</p>
+            <p><strong>Друзья:</strong> {friends}</p>
+            <p><strong>Суперсила:</strong> {superpowers}</p>
             <img src={url} alt={name}></img>
-            <p><strong>Информация:</strong>{info}</p>
+            <p><strong>Информация:</strong> {info}</p>
         </div>
     )
 }
